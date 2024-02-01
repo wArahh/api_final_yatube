5f82147506b079d1a8d63d131ec4da836500f9f1
olesh <o@i>
2024-02-01 16:39:54 +0300

    changes

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import permissions, status, filters, viewsets, mixins
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 
 from posts.models import Comment, Group, Post
