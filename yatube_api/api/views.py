dfbe7fd62128779996e9c80459e47516a956880a
olesh <o@i>
2024-02-01 16:46:46 +0300

    changes

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -12,6 +12,11 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
         if instance.author != self.request.user:
