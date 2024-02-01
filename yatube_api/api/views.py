1704600ebb78b0a18b5e3f454f51a941da49ffc6
olesh <o@i>
2024-02-01 16:49:41 +0300

    changes

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import permissions, status, filters, viewsets, mixins
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 
 from posts.models import Comment, Group, Post
@@ -11,6 +12,7 @@ from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    pagination_class = LimitOffsetPagination
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
