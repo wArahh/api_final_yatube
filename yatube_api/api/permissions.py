7c7555f6f27c4e662eb19bdc19b20a039b7e4081
olesh <w@w>
2024-02-01 22:35:36 +0300

    so good

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,6 +2,12 @@ from rest_framework import permissions
 
 
 class AuthorOrSafePermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
