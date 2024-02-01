abc6ec9e76e6c79f03a1035bf61c384ad74d6a56
olesh <o@i>
2024-02-01 18:52:39 +0300

    changes

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 
 from posts.models import Comment, Follow, Group, Post, User
+from rest_framework.exceptions import ValidationError
 from rest_framework.fields import CurrentUserDefault
 from rest_framework.validators import UniqueTogetherValidator
 
 already_follow = 'Вы уже подписаны на этого пользователя.'
+cannot_follow_yourself = 'Нельзя подписаться на себя'
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -49,6 +51,13 @@ class FollowSerializer(serializers.ModelSerializer):
         fields=('user', 'following',),
         message=already_follow,
     ),
+
+    def validate(self, data):
+        if self.context['request'].user == data['following']:
+            raise ValidationError(
+                cannot_follow_yourself
+            )
+        return data
 
     class Meta:
         model = Follow
